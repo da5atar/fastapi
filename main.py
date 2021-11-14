f919fb231516b054ab963b1397428b79c5d330b9
Massamba Sow <mass.sowd@gmail.com>
2021-11-14 18:17:00 -0500

    Add first routes

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to my API!!"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "These are your posts."}
+
+
+@app.post("/createpost")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"message": "Post created successfully."}
