5816e33853f667568d419fbdcfadb63af216ff59
Massamba Sow <mass.sowd@gmail.com>
2021-11-28 14:26:58 -0500

    cleanup - checkpoint

--- app/main.py
+++ app/main.py
@@ -14,39 +14,6 @@ app = FastAPI()
 # db
 models.Base.metadata.create_all(bind=engine)
 
-
-while True:
-    try:
-        conn = psycopg2.connect(
-            host="localhost",
-            database="fastapi",
-            user="postgres",
-            password="postgres",
-            cursor_factory=RealDictCursor,
-        )
-        print("Connected to database")
-        cursor = conn.cursor()
-        break
-    except Exception as error:
-        print("Unable to connect to the database")
-        print("Error:", error)
-        sleep(5)
-        continue
-
-
-# test endpoint
-@app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
-    try:
-        posts_query = db.query(models.Post)
-        posts = posts_query.all()
-        print(posts_query)
-        return {"status": "success", "data": posts}
-    except Exception as error:
-        print(error)
-        return {"status": "error"}
-
-
 # routes (endpoints)
 @app.get("/")
 async def root():
@@ -107,52 +74,4 @@ def update_post(post_id: int, post: schemas.UpdatePost, db: Session = Depends(ge
     post_query.update(post.dict(), synchronize_session=False)
     db.commit()
     updated_post = post_query.first()
-    return updated_post
-
-
-# helper functions
-def get_posts_from_db():
-    cursor.execute("SELECT * FROM posts")
-    posts = cursor.fetchall()
-    return posts
-
-
-def get_post_from_db(post_id):
-    cursor.execute(
-        "SELECT * FROM posts WHERE id = %s", (post_id,)
-    )  # comma after post_id is needed for single value
-    post = cursor.fetchone()
-    return post
-
-
-def create_post_in_db(post: schemas.Post):
-    cursor.execute(
-        "INSERT INTO posts (title, content, is_published, rating) VALUES (%s, %s, %s, %s) RETURNING *",
-        (post.title, post.content, post.is_published, post.rating),
-    )
-    created_post = cursor.fetchone()
-    conn.commit()
-    return {"Post created": created_post}
-
-
-def delete_post_in_db(post_id):
-    cursor.execute("DELETE FROM posts WHERE id = %s RETURNING *", (post_id,))
-    deleted_post = cursor.fetchone()
-    conn.commit()
-    return deleted_post
-
-
-def update_post_in_db(post_id, post_update):
-    cursor.execute(
-        "UPDATE posts SET title = %s, content = %s, is_published = %s, rating = %s WHERE id = %s RETURNING *",
-        (
-            post_update.title,
-            post_update.content,
-            post_update.is_published,
-            post_update.rating,
-            post_id,
-        ),
-    )
-    updated_post = cursor.fetchone()
-    conn.commit()
     return updated_post
